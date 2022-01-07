362538a000ae585f3ab8ae15d91e427bce36d0d4
perniKoder <pernilleaaby@outlook.com>
2022-01-07 13:09:25 +0100

    Back to prod

--- embedding_generation/generate_averaged_embeddings.py
+++ embedding_generation/generate_averaged_embeddings.py
@@ -67,7 +67,7 @@ def save_word_embeddings(word_embedding_dict):
 
 word_embedding_dict = {}
 
-for j, chunk in enumerate(text_chunks[:300]): 
+for j, chunk in enumerate(text_chunks): 
     chunk = chunk.strip()
     word_array = [token.text for token in nlp(chunk)]
     token_mapping, token_embeddings = tokens_to_embeddings(word_array, tokenizer, bert_model)
